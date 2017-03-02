7650ab85fab05393d1f5cbcf85ee9bcac3e4c478
Anton Popov <popovanton567@gmail.com>
2017-03-02 18:31:25 +0100

    feat: Add bugs in third ex

--- homework_1/exercise_3.py
+++ homework_1/exercise_3.py
@@ -1,8 +1,23 @@
 import numpy as np
 from scipy import linalg
+import matplotlib.pyplot as plt
 
 p = np.mat([[0.8, 0.1, 0.1],[0.2, 0.0, 0.8],[0.4, 0.4, 0.2]])
 
 # Verify by simulation recurrent and transient states
 x = np.mat([1.0, 0.0, 0.0])
-print(x * p)
+
+def step(p, state):
+    # sum_prob[i] contains sum: p[state, 0] + p[state, 1] + ... + [pstate, i]
+    sum_prob = []
+    for j in range(p.shape[1]):
+        sum = 0
+        for k in range(0, j):
+            print(k, )
+            sum += p[state,k]
+        sum_prob.append(sum)
+    print(sum_prob)
+
+
+for el in range(3):
+    step(p, el)
